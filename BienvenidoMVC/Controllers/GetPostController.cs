df422f9a07161cf204cd07b88c8dc51884b81b51
Guillermo Castillo <guillermo.castillo@ingeniosys.net>
2020-08-20 15:58:50 -0500

    Prueba de Page GitHub

---
+++ BienvenidoMVC/Controllers/GetPostController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BienvenidoMVC.Controllers
+{
+    public class GetPostController : Controller
+    {
+        // GET: GetPost
+        //public ActionResult Index(string parNombreUsuario, string parPwd)
+        //{
+        //    ViewBag.NombreUsuario = parNombreUsuario;
+        //    ViewBag.Pwd = parPwd;
+        //    return View("Submit");
+        //}
+
+        [HttpGet]
+        public ActionResult Submit(string parNombreUsuario, string parPwd)
+        {
+            ViewBag.NombreUsuario = parNombreUsuario;
+            ViewBag.Pwd = parPwd;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(FormCollection objColl)
+        {
+            ViewBag.NombreUsuario = objColl["parNombreUsuario"];
+            ViewBag.Pwd = objColl["parPwd"];
+            return View("Index");
+        }
+
+    }
+}
---
+++ BienvenidoMVC/Models/ClsLoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BienvenidoMVC.Models
+{
+    public class ClsLoginModel
+    {
+        public string NombreUsuario { get; set; }
+        public string Pwd { get; set; }
+    }
+}
